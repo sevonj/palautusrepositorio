e98ab11a5ce313a1a8b6e09c84ecf8223554667c
sevonj <100710152+sevonj@users.noreply.github.com>
2023-11-27 20:50:55 +0200

    vk4-t4

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -147,4 +147,89 @@ class TestKauppa(unittest.TestCase):
         pankki_mock.tilisiirto.assert_called_with("jörg", 52, "555", "33333-44455", 5)
         #nimi, viitenumero, tililta, tilille, summa
 
-    
+    def test_aloita_nollaa_kaupan(self):
+        pankki_mock = Mock()
+        viitegeneraattori_mock = Mock()
+
+        # palautetaan aina arvo 42
+        viitegeneraattori_mock.uusi.return_value = 52
+
+        varasto_mock = Mock()
+
+        # tehdään toteutus saldo-metodille
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+            if tuote_id == 2:
+                return 0
+
+        # tehdään toteutus hae_tuote-metodille
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "orangensaft", 5)
+            if tuote_id == 2:
+                return Tuote(2, "kalja", 3)
+
+        # otetaan toteutukset käyttöön
+        varasto_mock.saldo.side_effect = varasto_saldo
+        varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+        kauppa = Kauppa(varasto_mock, pankki_mock, viitegeneraattori_mock)
+        
+        # Tapahtumia
+
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.lisaa_koriin(2)
+        kauppa.tilimaksu("jörg", "555")
+        viitegeneraattori_mock.uusi.assert_has_calls([()])
+
+        # Nollaus
+        kauppa.aloita_asiointi()
+        # Testaus
+        kauppa.tilimaksu("jörg", "555")
+        viitegeneraattori_mock.uusi.assert_has_calls([(),()])
+        pankki_mock.tilisiirto.assert_called_with("jörg", 52, "555", "33333-44455", 0)
+
+        # Ostamattomia tapahtumia
+        kauppa.lisaa_koriin(1)
+        kauppa.lisaa_koriin(2)
+        # Nollaus
+        kauppa.aloita_asiointi()
+        # Testaus
+        kauppa.tilimaksu("jörg", "555")
+        viitegeneraattori_mock.uusi.assert_has_calls([(),(),()])
+        pankki_mock.tilisiirto.assert_called_with("jörg", 52, "555", "33333-44455", 0)
+
+    def test_poistakorista(self):
+        pankki_mock = Mock()
+        viitegeneraattori_mock = Mock()
+
+        # palautetaan aina arvo 42
+        viitegeneraattori_mock.uusi.return_value = 52
+
+        varasto_mock = Mock()
+
+        # tehdään toteutus saldo-metodille
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+
+        # tehdään toteutus hae_tuote-metodille
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "maito", 5)
+
+        # otetaan toteutukset käyttöön
+        varasto_mock.saldo.side_effect = varasto_saldo
+        varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+        kauppa = Kauppa(varasto_mock, pankki_mock, viitegeneraattori_mock)
+        
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.poista_korista(1)
+        kauppa.tilimaksu("geoff", "555")
+
+        pankki_mock.tilisiirto.assert_called_with("geoff", 52, "555", "33333-44455", 0)
+        #nimi, viitenumero, tililta, tilille, summa
