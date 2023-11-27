f30dc291aa810d6fedcb9dae31c6a60f769aedc7
sevonj <100710152+sevonj@users.noreply.github.com>
2023-11-27 20:07:41 +0200

    vk4-t2

--- viikko4/maksukortti-mock/src/kassapaate.py
+++ viikko4/maksukortti-mock/src/kassapaate.py
@@ -6,8 +6,21 @@ class Kassapaate:
         self.myytyja_lounaita = 0
 
     def lataa(self, kortti, summa):
+        if summa < 0:
+            return
+
         kortti.lataa(summa)
 
     def osta_lounas(self, kortti):
+        # "Maksukortin tilaa ei myöskään ole tarkoitus tutkia suoraan, koska
+        # Maksukortti on mock ei attribuuttien arvojen katsominen edes ole
+        # mahdollista/mielekästä."
+
+        # En kyllä keksi tähän mitään muuta keinoa, kuin lukea kortin tila
+        # suoraan.
+
+        if kortti.saldo < HINTA:
+            return
+
         kortti.osta(HINTA)
         self.myytyja_lounaita = self.myytyja_lounaita + 1
--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -11,7 +11,7 @@ class TestKassapaate(unittest.TestCase):
     def test_kortilta_velotetaan_hinta_jos_rahaa_on(self):
         maksukortti_mock = Mock()
         maksukortti_mock.saldo = 10
-        
+
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_called_with(HINTA)
@@ -19,7 +19,23 @@ class TestKassapaate(unittest.TestCase):
     def test_kortilta_ei_veloteta_jos_raha_ei_riita(self):
         maksukortti_mock = Mock()
         maksukortti_mock.saldo = 4
-        
+
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lataus_kortille_oikea_summa(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 4
+
+        self.kassa.lataa(maksukortti_mock, 3)
+
+        maksukortti_mock.lataa.assert_called_with(3)
+
+    def test_neg_lataus_kortille_eiteemitään(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 4
+
+        self.kassa.lataa(maksukortti_mock, -3)
+
+        maksukortti_mock.lataa.assert_not_called()
